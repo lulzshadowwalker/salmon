c094554ef567457861238760298a205f0617529f
lulzshadowwalker <lulzshadowwalker@gmail.com>
2023-09-22 11:40:50 +0300

    system statusbar now responds to theme changes

--- lib/theme/salmon_theme.dart
+++ lib/theme/salmon_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -101,6 +102,7 @@ final class SalmonTheme {
         elevation: 0,
         centerTitle: true,
         surfaceTintColor: Colors.transparent,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
       ),
 
       //
@@ -165,6 +167,7 @@ final class SalmonTheme {
         elevation: 0,
         centerTitle: true,
         surfaceTintColor: Colors.transparent,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
       ),
 
       //
